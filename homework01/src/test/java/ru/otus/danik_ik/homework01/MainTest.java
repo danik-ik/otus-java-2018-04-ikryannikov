880f20306e1fbcb78beca0eaed564c26ee4ec0c0
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-04-14 12:41:00 +0300

    Добавлен голый проект для ДЗ

---
+++ homework01/src/test/java/ru/otus/danik_ik/homework01/MainTest.java
@@ -0,0 +1,47 @@
+package ru.otus.danik_ik.homework01;
+
+import java.lang.String;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Simple parametrized test.
+ */
+@RunWith(Parameterized.class)
+public class MainTest
+{
+  /**
+   * Data for test.
+   */
+  @Parameterized.Parameters(name = "{index}: («{1}»)")
+  public static Iterable<Object[]> dataForTest() {
+    return Arrays.asList(new Object[][]{
+      /*  uncomment the lines below to create test cases
+          each row used as set of parameters for constructor */
+//      {"AAA", "AAA"},
+//      {"BBB", "bbb"},
+//      {"CCC", "Ccc"},
+    });
+  }
+
+  /**
+   * Data for current instance (one row from result of dataForTest() )
+   */
+  private String expected;
+  private String input;
+
+  public MainTest(String expected, String input) {
+    this.expected = expected;
+    this.input = input;
+  }
+
+  @Test
+  public void testUppercase() throws Exception {
+    assertEquals(expected, input.toUpperCase());
+  }
+}
