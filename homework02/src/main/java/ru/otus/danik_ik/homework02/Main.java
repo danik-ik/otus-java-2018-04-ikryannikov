40559793bb93ab6aeab7c1550f734839f241e1a6
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-04-21 15:18:47 +0300

    Добавлена болванка ДЗ

---
+++ homework02/src/main/java/ru/otus/danik_ik/homework02/Main.java
@@ -0,0 +1,13 @@
+package ru.otus.danik_ik.homework02;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+        {
+            System.out.println( "Hello World!" );
+        }
+}
