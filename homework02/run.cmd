497acffd052763bbb97ce58be4f26cdd6f77ce30
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-04-21 15:28:36 +0300

    пакетник пока для запуска, чтобы не мучиться

---
+++ homework02/run.cmd
@@ -0,0 +1,2 @@
+@ echo off
+java -javaagent:target/homework02-1.0-SNAPSHOT-jar-with-dependencies.jar -jar target/homework02-1.0-SNAPSHOT-jar-with-dependencies.jar
--- homework02/src/main/java/ru/otus/danik_ik/homework02/Main.java
+++ homework02/src/main/java/ru/otus/danik_ik/homework02/Main.java
@@ -7,7 +7,7 @@ package ru.otus.danik_ik.homework02;
 public class Main
 {
     /*
-        запускать jar сборки, указывая его же в параметре javaagent
+        запускать jar сборки, указывая его же в параметре javaagent (см. run.cmd)
      */
     public static void main( String[] args )
         {
