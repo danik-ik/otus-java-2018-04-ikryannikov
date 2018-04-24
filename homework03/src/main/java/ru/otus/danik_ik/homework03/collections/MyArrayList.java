080570265b7bfe2242c31e38d89597e04e8d2d2d
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-04-24 14:01:33 +0300

    Наброски

--- homework03/src/main/java/ru/otus/danik_ik/homework03/collections/MyArrayList.java
+++ homework03/src/main/java/ru/otus/danik_ik/homework03/collections/MyArrayList.java
@@ -1,123 +1,138 @@
 package ru.otus.danik_ik.homework03.collections;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.*;
 
 public class MyArrayList<E> implements List<E> {
-  @Override
-  public int size() {
-    return 0;
-  }
+    private E[] data;
+    private final int DEFAULT_CAPACITY = 5;
+    private int count = 0;
+    private int capacity;
 
-  @Override
-  public boolean isEmpty() {
-    return false;
-  }
+    public MyArrayList() {
+        initData(DEFAULT_CAPACITY);
+    }
 
-  @Override
-  public boolean contains(Object o) {
-    return false;
-  }
+    public MyArrayList(int capacity) {
+        initData(capacity);
+    }
 
-  @Override
-  public Iterator<E> iterator() {
-    return null;
-  }
+    private void initData(int initial_capacity) {
+        data = (E[]) new Object[initial_capacity];
+        capacity = initial_capacity;
+    }
 
-  @Override
-  public Object[] toArray() {
-    return new Object[0];
-  }
+    @Override
+    public int size() {
+        return 0;
+    }
 
-  @Override
-  public <T> T[] toArray(T[] a) {
-    return null;
-  }
+    @Override
+    public boolean isEmpty() {
+        return 0 == size();
+    }
 
-  @Override
-  public boolean add(E e) {
-    return false;
-  }
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
 
-  @Override
-  public boolean remove(Object o) {
-    return false;
-  }
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
 
-  @Override
-  public boolean containsAll(Collection<?> c) {
-    return false;
-  }
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
 
-  @Override
-  public boolean addAll(Collection<? extends E> c) {
-    return false;
-  }
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
 
-  @Override
-  public boolean addAll(int index, Collection<? extends E> c) {
-    return false;
-  }
+    @Override
+    public boolean add(E e) {
+        return false;
+    }
 
-  @Override
-  public boolean removeAll(Collection<?> c) {
-    return false;
-  }
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
 
-  @Override
-  public boolean retainAll(Collection<?> c) {
-    return false;
-  }
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
 
-  @Override
-  public void clear() {
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
 
-  }
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
 
-  @Override
-  public E get(int index) {
-    return null;
-  }
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
 
-  @Override
-  public E set(int index, E element) {
-    return null;
-  }
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
 
-  @Override
-  public void add(int index, E element) {
+    @Override
+    public void clear() {
 
-  }
+    }
 
-  @Override
-  public E remove(int index) {
-    return null;
-  }
+    @Override
+    public E get(int index) {
+        return null;
+    }
 
-  @Override
-  public int indexOf(Object o) {
-    return 0;
-  }
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
 
-  @Override
-  public int lastIndexOf(Object o) {
-    return 0;
-  }
+    @Override
+    public void add(int index, E element) {
 
-  @Override
-  public ListIterator<E> listIterator() {
-    return null;
-  }
+    }
 
-  @Override
-  public ListIterator<E> listIterator(int index) {
-    return null;
-  }
+    @Override
+    public E remove(int index) {
+        return null;
+    }
 
-  @Override
-  public List<E> subList(int fromIndex, int toIndex) {
-    return null;
-  }
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
 }
--- homework03/src/test/java/ru/otus/danik_ik/homework03/collections/MyArrayListTest.java
+++ homework03/src/test/java/ru/otus/danik_ik/homework03/collections/MyArrayListTest.java
@@ -3,12 +3,14 @@ package ru.otus.danik_ik.homework03.collections;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static junit.framework.TestCase.*;
 //import static org.junit.Assert.assertEquals;
 
 public class MyArrayListTest {
+    private ArrayList l;
     private List<String> list;
     @Before
     public void setup() {
@@ -25,5 +27,16 @@ public class MyArrayListTest {
         assertTrue(list.isEmpty());
     }
     
-    
+    @Test
+    public void size1() {
+        list.add("");
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void emptyFalse() {
+        list.add("");
+        assertFalse(list.isEmpty());
+    }
+
 }
