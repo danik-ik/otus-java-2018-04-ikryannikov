e4e72675c66cf97044f10d438a89e6c837704c57
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-01 13:57:49 +0300

    Добавлен исходник задания -- класс, который надо доделать

--- homework02a/src/main/java/ru/otus/danik_ik/homework02a/Main.java
+++ homework02a/src/main/java/ru/otus/danik_ik/homework02a/Main.java
@@ -3,6 +3,7 @@ package ru.otus.danik_ik.homework02a;
 public class Main
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
+        new ArrayCopyTutor().testAnimals();
     }
 }
