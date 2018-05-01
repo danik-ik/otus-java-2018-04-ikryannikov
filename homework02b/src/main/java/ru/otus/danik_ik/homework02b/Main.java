96c054da220012dbcb4dd20f1184f38c4ce0d1cf
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-01 16:26:49 +0300

    Чтобы никто не сомневался, комментарий в main

--- homework02b/src/main/java/ru/otus/danik_ik/homework02b/Main.java
+++ homework02b/src/main/java/ru/otus/danik_ik/homework02b/Main.java
@@ -3,6 +3,6 @@ package ru.otus.danik_ik.homework02b;
 public class Main
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Здесь ничего нет. Смотри на тесты" );
     }
 }
