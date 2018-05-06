1b7513c6fa83bde6a6bb5a6316e68403a5b6e8ea
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-06 21:11:16 +0300

    Первая попытка слежения за сборкой мусора

--- homework04/src/main/java/ru/otus/danik_ik/homework04/Main.java
+++ homework04/src/main/java/ru/otus/danik_ik/homework04/Main.java
@@ -1,8 +1,30 @@
 package ru.otus.danik_ik.homework04;
 
+import javax.management.MBeanServer;
+import java.lang.management.GarbageCollectorMXBean;
+import java.lang.management.ManagementFactory;
+import java.util.List;
+
 public class Main
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        new Main().loop();
+    }
+
+    @SuppressWarnings("InfiniteLoopStatement")
+    private void loop() {
+        while (true) {
+            int[] a = new int[1_000_000];
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            List<GarbageCollectorMXBean> gcs = ManagementFactory.getGarbageCollectorMXBeans();
+            System.out.println("============================");
+            for (GarbageCollectorMXBean gc : gcs) {
+                System.out.printf("Name: %s; count: %s; time: %s\n", gc.getName(), gc.getCollectionCount(), gc.getCollectionTime());
+            }
+        }
     }
 }
