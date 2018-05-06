3be24630edd438f0b77cec28b4c5e4448c23685c
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-06 21:54:52 +0300

    Утечка памяти

--- homework04/src/main/java/ru/otus/danik_ik/homework04/Main.java
+++ homework04/src/main/java/ru/otus/danik_ik/homework04/Main.java
@@ -2,24 +2,35 @@ package ru.otus.danik_ik.homework04;
 
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import static java.lang.Thread.sleep;
 
 public class Main
 {
+    public static final int DELAY_MILLIS = 500;
+    public static final int BIG_OBJECT_SIZE = 20_000;
+
     public static void main( String[] args ) throws InterruptedException {
-        new Main().loop();
+        new Main().execute();
+    }
+
+    private void execute() throws InterruptedException {
+        int leakCountAtStep = 10;
+        loop(leakCountAtStep);
     }
 
     long startTime = System.nanoTime();
+    List<long[]> list = new LinkedList<>();
 
     @SuppressWarnings("InfiniteLoopStatement")
-    private void loop() throws InterruptedException {
+    private void loop(int leakCountAtStep) throws InterruptedException {
         while (true) {
-            int[] a = new int[1_000_000];
+            for (int i = 0; i < leakCountAtStep; i++) leak();
             printStat();
-            sleep(10);
+            sleep(DELAY_MILLIS);
         }
     }
 
@@ -33,6 +44,14 @@ public class Main
                     gc.getCollectionTime());
         }
         System.out.printf("Free memory: %d/%d MiB\n ", rt.freeMemory()/1024/1024, rt.totalMemory()/1024/1024);
+    }
+
+    private void leak() {
+        list.add(new long[BIG_OBJECT_SIZE]);
+        list.add(new long[BIG_OBJECT_SIZE]);
+        list.add(new long[BIG_OBJECT_SIZE]);
+        list.remove(0);
+        list.remove(0);
     }
 
 }
