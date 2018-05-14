d9de27607f468257c3ee567fd4253fed727e480f
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-14 22:00:01 +0300

    Первый класс для тестирования

---
+++ homework05/src/test/java/ru/otus/danik_ik/homework05/testo/testpackage1/Class1.java
@@ -0,0 +1,10 @@
+package ru.otus.danik_ik.homework05.testo.testpackage1;
+
+import ru.otus.danik_ik.homework05.testo.*;
+
+public class Class1 {
+    @Test
+    public void Method1() {
+        Counter.CountTest();
+    };
+}
