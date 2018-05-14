5365b363e3d8150ea7c5e439007eae1f38f66860
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-14 22:36:28 +0300

    Каркас реализации

--- homework05/src/main/java/ru/otus/danik_ik/homework05/testo/Engine.java
+++ homework05/src/main/java/ru/otus/danik_ik/homework05/testo/Engine.java
@@ -1,7 +1,29 @@
 package ru.otus.danik_ik.homework05.testo;
 
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 public class Engine {
+    private final String target;
+
     public static void execute(String target) {
-        ////
+        new Engine(target);
+    }
+
+    private Engine(String target) {
+        this.target = target;
+        if ( runAsPackage() ) return;
+        if ( runAsClass() ) return;
+        System.out.println("Параметр должен быть полным именем класса или пакета");
+    };
+
+    boolean runAsPackage() {
+        return false;
+    }
+
+    boolean runAsClass() {
+        return false;
     }
 }
