653ea3d60054c21c8e4a312076e9eba0e89a8413
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-16 21:39:00 +0300

    Добавлено исключение при выполнении тестов

---
+++ homework05/src/main/java/ru/otus/danik_ik/homework05/testo/TestExecutionException.java
@@ -0,0 +1,7 @@
+package ru.otus.danik_ik.homework05.testo;
+
+public class TestExecutionException extends Exception {
+    public TestExecutionException(Throwable cause) {
+        super(cause);
+    }
+}
