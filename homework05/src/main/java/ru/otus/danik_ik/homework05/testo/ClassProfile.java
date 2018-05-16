9fa87bf9e779e9737e6abe79facf31d3da6e77f2
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-16 21:22:35 +0300

    Начинает оформляться

--- homework05/src/main/java/ru/otus/danik_ik/homework05/testo/ClassProfile.java
+++ homework05/src/main/java/ru/otus/danik_ik/homework05/testo/ClassProfile.java
@@ -1,23 +1,57 @@
 package ru.otus.danik_ik.homework05.testo;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClassProfile {
+    private final Class target;
+    private final Constructor defaultConstructor;
     private Method beforeMethod;
     private Method afterMethod;
     private List<Method> testMethods = new ArrayList<>();
     private Object instance;
 
     public ClassProfile(Class target) {
-        // TODO: 16.05.2018 разобрать методы по аннотациям
+        Constructor defaultConstructor;
+        this.target = target;
+        try {
+            defaultConstructor = target.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            defaultConstructor = null;
+        }
+        this.defaultConstructor = defaultConstructor;
+        if (this.defaultConstructor == null) return;
+
+        analizeAnnotations();
     }
 
-    public boolean isTest() { return !testMethods.isEmpty(); }
+    private void analizeAnnotations() {
 
-    public void createInstance() {}
-    public void executeBefore() {}
-    public void executeTests() {}
-    public void executeAfter() {}
+    }
+
+    public boolean isTest() {
+        return defaultConstructor != null && !testMethods.isEmpty();
+    }
+
+    public void executeTests() throws IllegalAccessException, InstantiationException, InvocationTargetException {
+        createInstance();
+        executeBefore();
+        executeEachTest();
+        executeAfter();
+    }
+
+    private void createInstance() throws IllegalAccessException, InvocationTargetException, InstantiationException {
+        try {
+            Constructor constructor = target.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+        instance = defaultConstructor.newInstance();
+    }
+    private void executeBefore() {}
+    private void executeEachTest() {}
+    private void executeAfter() {}
 }
