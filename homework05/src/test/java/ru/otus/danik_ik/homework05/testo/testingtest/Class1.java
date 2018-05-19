724acf5b99660f5c5db587ffa689a0cc43eeb106
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-19 22:59:27 +0300

    Удалён наконец-то безумный статический Counter

--- homework05/src/test/java/ru/otus/danik_ik/homework05/testo/testingtest/Class1.java
+++ homework05/src/test/java/ru/otus/danik_ik/homework05/testo/testingtest/Class1.java
@@ -5,21 +5,17 @@ import ru.otus.danik_ik.homework05.testo.*;
 public class Class1 {
     @Before
     public void before() {
-        Counter.countBefore();
     }
 
     @After
     public void after() {
-        Counter.countAfter();
     }
 
     @Test
     public void test1() {
-        Counter.countTest();
     }
 
     @Test
     public void test2() {
-        Counter.countTest();
     }
 }
--- homework05/src/test/java/ru/otus/danik_ik/homework05/testo/testingtest/Class2.java
+++ homework05/src/test/java/ru/otus/danik_ik/homework05/testo/testingtest/Class2.java
@@ -1,27 +1,22 @@
 package ru.otus.danik_ik.homework05.testo.testingtest;
 
 import ru.otus.danik_ik.homework05.testo.After;
-import ru.otus.danik_ik.homework05.testo.Counter;
 import ru.otus.danik_ik.homework05.testo.Test;
 
 public class Class2 {
     @After
     public void after() {
-        Counter.countAfter();
     }
 
     @Test
     public void test1() {
-        Counter.countTest();
     }
 
     @Test
     public void test2() {
-        Counter.countTest();
     }
 
     @Test
     public void test3() {
-        Counter.countTest();
     }
 }
