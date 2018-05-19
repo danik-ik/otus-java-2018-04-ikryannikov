ed0a920b4e8ae50aa010e80be68ab8f01effd74c
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-19 19:08:35 +0300

    Упростил Main. Будет отдельный поставщик классов

--- homework05/src/main/java/ru/otus/danik_ik/homework05/Main.java
+++ homework05/src/main/java/ru/otus/danik_ik/homework05/Main.java
@@ -1,27 +1,23 @@
 package ru.otus.danik_ik.homework05;
 
 import ru.otus.danik_ik.homework05.testo.Engine;
-import ru.otus.danik_ik.homework05.testo.TargetNotFoundException;
 import ru.otus.danik_ik.homework05.testo.TestException;
 
 public class Main
 {
-    private static final String TARGET1 = "ru.otus.danik_ik.homework05.testo.test1";
-    private static final String TARGET1_CLASS = "ru.otus.danik_ik.homework05.testo.test1.Class1";
-    private static final String TARGET2 = "ru.otus.danik_ik.homework05.testo.test2.Class1";
-    private static final String TARGET3 = "ru.otus.danik_ik.homework05.testo.test1.Class2";
+    private static final String TARGETS[] = {
+            "ru.otus.danik_ik.homework05.testo.test1",
+            "ru.otus.danik_ik.homework05.testo.test2.Class1",
+            "ru.otus.danik_ik.homework05.testo.test1.Class2",
+    };
 
     public static void main( String[] args ) throws ClassNotFoundException {
-        // Предварительно загружается класс, иначе не будет доступен пакет
-        Class.forName(TARGET1_CLASS);
-        test(TARGET1, "package");
-        test(TARGET2, "class");
-        test(TARGET3, "class");
+        for (String target: TARGETS) test(target);
     }
 
-    private static void test(String target, String comment) {
+    private static void test(String target) {
         System.out.println("=================================================");
-        System.out.printf("%s: %s\n", comment, target);
+        System.out.println(target);
         System.out.println("=================================================");
 
         try {
