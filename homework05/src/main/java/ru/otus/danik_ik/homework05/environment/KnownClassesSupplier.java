1f9804c69a94e094eca1a5a45a1bde3798215ef0
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-20 22:02:37 +0300

    К списку классов добавлен класс без конструктора по умолчанию.

--- homework05/src/main/java/ru/otus/danik_ik/homework05/environment/KnownClassesSupplier.java
+++ homework05/src/main/java/ru/otus/danik_ik/homework05/environment/KnownClassesSupplier.java
@@ -12,6 +12,7 @@ public class KnownClassesSupplier implements ClassesSupplier {
             "ru.otus.danik_ik.homework05.testo.test1.NoAfter",
             "ru.otus.danik_ik.homework05.testo.test1.NoBefore",
             "ru.otus.danik_ik.homework05.testo.test1.WithParams",
+            "ru.otus.danik_ik.homework05.testo.test1.NoDefaultConstructor",
             "ru.otus.danik_ik.homework05.testo.test1.NoTest",
             "ru.otus.danik_ik.homework05.testo.test2.Class1",
     });
