2475bafe19d7513549635bfb3c15f36631933718
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-20 19:57:02 +0300

    Оформлен вывод информации о параметрах запуска

--- homework05/src/main/java/ru/otus/danik_ik/homework05/Main.java
+++ homework05/src/main/java/ru/otus/danik_ik/homework05/Main.java
@@ -9,14 +9,20 @@ import ru.otus.danik_ik.homework05.testo.TestException;
 public class Main
 {
     private static TestEnvironment environment = new SimpleTestEnvironment(new KnownClassesSupplier());
-    private static final String TARGETS[] = {
-            "ru.otus.danik_ik.homework05.testo.test1",
-            "ru.otus.danik_ik.homework05.testo.test2.Class1",
-            "ru.otus.danik_ik.homework05.testo.test1.Class2",
+    private static final String TARGETS[][] = {
+            {"ru.otus.danik_ik.homework05.testo.test1","Пакет"},
+            {"ru.otus.danik_ik.homework05.testo.test2.Class1","Класс"},
+            {"ru.otus.danik_ik.homework05.testo.test1.Class2","Класс"},
     };
 
     public static void main( String[] args ) throws ClassNotFoundException {
-        for (String target: TARGETS) test(target);
+        for (String[] target: TARGETS) {
+            System.out.println();
+            System.out.println("*****************************************************************************");
+            System.out.println("запуск с параметром " + target[0] + " (" + target[1] + ")");
+            System.out.println("*****************************************************************************");
+            test(target[0]);
+        }
     }
 
     private static void test(String target) {
