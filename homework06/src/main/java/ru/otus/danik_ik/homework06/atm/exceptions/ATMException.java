2d7a158589d0b8d273dc6c9a4b550da85baf269b
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-22 10:53:01 +0300

    Добавлены конструкторы к исключениям

--- homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/ATMException.java
+++ homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/ATMException.java
@@ -1,4 +1,11 @@
 package ru.otus.danik_ik.homework06.atm.exceptions;
 
 public class ATMException extends Exception {
+    public ATMException(Throwable e) {
+        super(e);
+    }
+
+    public ATMException() {
+        super();
+    }
 }
--- homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/AmountCantBeCollectedException.java
+++ homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/AmountCantBeCollectedException.java
@@ -1,4 +1,11 @@
 package ru.otus.danik_ik.homework06.atm.exceptions;
 
 public class AmountCantBeCollectedException extends ATMException {
+    public AmountCantBeCollectedException(Throwable e) {
+        super(e);
+    }
+
+    public AmountCantBeCollectedException() {
+        super();
+    }
 }
