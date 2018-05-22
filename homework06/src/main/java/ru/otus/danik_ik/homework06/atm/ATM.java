b029cf22851a47f9b845e45ec67bcf178dd17424
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-22 09:28:46 +0300

    Разделены сумма к выдаче и общая, добавлено исключение к методу deposit()

--- homework06/src/main/java/ru/otus/danik_ik/homework06/atm/ATM.java
+++ homework06/src/main/java/ru/otus/danik_ik/homework06/atm/ATM.java
@@ -1,13 +1,15 @@
 package ru.otus.danik_ik.homework06.atm;
 
 import ru.otus.danik_ik.homework06.atm.exceptions.AmountCantBeCollectedException;
+import ru.otus.danik_ik.homework06.atm.exceptions.CantDepositException;
 import ru.otus.danik_ik.homework06.money.Banknote;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public interface ATM {
-    public void deposit(List<Banknote> bundle);
+    public void deposit(List<Banknote> bundle) throws CantDepositException;
     public List<Banknote> withdraw(BigDecimal amount) throws AmountCantBeCollectedException;
-    public BigDecimal getAmount();
+    public BigDecimal getAmountToIssue();
+    public BigDecimal getAmountTotal();
 }
