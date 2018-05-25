5af6586c5dd69a1fd537ffe49a217d0df75eab23
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-05-25 10:36:53 +0300

    Реализована выдача. Тест прошёл

--- homework06/src/main/java/ru/otus/danik_ik/homework06/RecyclableATM.java
+++ homework06/src/main/java/ru/otus/danik_ik/homework06/RecyclableATM.java
@@ -5,10 +5,13 @@ import ru.otus.danik_ik.homework06.atm.DepositCurrencyBox;
 import ru.otus.danik_ik.homework06.atm.WithdrawCurrencyBox;
 import ru.otus.danik_ik.homework06.atm.exceptions.AmountCantBeCollectedException;
 import ru.otus.danik_ik.homework06.atm.exceptions.CantDepositException;
+import ru.otus.danik_ik.homework06.atm.exceptions.NotEnoughException;
 import ru.otus.danik_ik.homework06.money.Banknote;
 
 import java.math.BigDecimal;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class RecyclableATM implements ATM {
     RecyclableCurrencyBox[] currencyBoxes = new RecyclableCurrencyBox[4];
@@ -33,7 +36,52 @@ public class RecyclableATM implements ATM {
 
     @Override
     public List<Banknote> withdraw(BigDecimal amount) throws AmountCantBeCollectedException {
-        return null;
+        List<WithdrawCurrencyBox> boxes = getOrderedWithdrawBoxes();
+
+        Map<WithdrawCurrencyBox, Integer> specification = getSpecification(boxes, amount.intValueExact());
+
+        return collectNotes(specification);
+    }
+
+    private List<Banknote> collectNotes(Map<WithdrawCurrencyBox,Integer> specification) throws AmountCantBeCollectedException {
+        List<Banknote> bundle = new ArrayList<>();
+        for (WithdrawCurrencyBox box: currencyBoxes)
+            if (specification.containsKey(box)) try {
+                bundle.addAll(box.withdraw(specification.get(box)));
+            } catch (NotEnoughException e) {
+                try {
+                    depositBox.deposit(bundle);
+                } catch (CantDepositException e1) {
+                    // Если при наборе пачки произошла ошибка, и мы не можем
+                    // сбросить эту пачку -- всё, сливай вода, глуши машина
+                    throw new RuntimeException(e1);
+                }
+                throw new AmountCantBeCollectedException(e);
+            }
+        return bundle;
+    }
+
+    private Map<WithdrawCurrencyBox,Integer> getSpecification(List<WithdrawCurrencyBox> boxes, int amount) throws AmountCantBeCollectedException {
+        int remainder = amount;
+        Map<WithdrawCurrencyBox,Integer> specification = new HashMap<>();
+        for (WithdrawCurrencyBox box: boxes) {
+            int denomination = box.getDenomination().asInt();
+            int requiredCount = remainder / denomination;
+            int count = box.canToWithdraw(requiredCount);
+            if (count > 0) {
+                remainder -= count * denomination;
+                specification.put(box, count);
+            }
+        }
+        if (remainder > 0) throw new AmountCantBeCollectedException();
+        return specification;
+    }
+
+    private List<WithdrawCurrencyBox> getOrderedWithdrawBoxes() {
+        return Stream.of(currencyBoxes)
+                .filter(it -> it != null)
+                .sorted(Comparator.comparing(a -> ((WithdrawCurrencyBox)a).getDenomination().asInt()).reversed())
+                .collect(Collectors.toList());
     }
 
     @Override
