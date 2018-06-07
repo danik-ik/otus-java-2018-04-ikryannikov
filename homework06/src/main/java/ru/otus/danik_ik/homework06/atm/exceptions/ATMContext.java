db6b065b79923c6744509f09bbb5911e991a0af9
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-07 14:09:50 +0300

    Интерфейсы для контекста

---
+++ homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/ATMContext.java
@@ -0,0 +1,7 @@
+package ru.otus.danik_ik.homework06.atm.exceptions;
+
+import ru.otus.danik_ik.homework06.money.BundleFactory;
+
+public interface ATMContext {
+    BundleFactory getBundleFactory();
+}
---
+++ homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/HasAtmContext.java
@@ -0,0 +1,6 @@
+package ru.otus.danik_ik.homework06.atm.exceptions;
+
+public interface HasAtmContext {
+    void setATMContext(ATMContext context);
+    ATMContext getAtmContext();
+}
