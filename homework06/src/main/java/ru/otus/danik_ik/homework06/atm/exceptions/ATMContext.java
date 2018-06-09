59300e6e0944a33aebc019cfe9cd4c2a9e4bdc34
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-09 14:16:46 +0300

    Revert "Интерфейсы для контекста"
    This reverts commit db6b065b79923c6744509f09bbb5911e991a0af9.

--- homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/ATMContext.java
+++
@@ -1,7 +0,0 @@
-package ru.otus.danik_ik.homework06.atm.exceptions;
-
-import ru.otus.danik_ik.homework06.money.BundleFactory;
-
-public interface ATMContext {
-    BundleFactory getBundleFactory();
-}
--- homework06/src/main/java/ru/otus/danik_ik/homework06/atm/exceptions/HasAtmContext.java
+++
@@ -1,6 +0,0 @@
-package ru.otus.danik_ik.homework06.atm.exceptions;
-
-public interface HasAtmContext {
-    void setATMContext(ATMContext context);
-    ATMContext getAtmContext();
-}
