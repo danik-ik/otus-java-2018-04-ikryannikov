94191b88637a14f43d1d9e19ea848713f607adca
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-10 22:42:49 +0300

    Фабрика для ATM принимает в параметре имя

--- homework07/src/main/java/ru/otus/danik_ik/homework07/Factories.java
+++ homework07/src/main/java/ru/otus/danik_ik/homework07/Factories.java
@@ -1,18 +1,19 @@
 package ru.otus.danik_ik.homework07;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class Factories {
-    private Supplier <RemoteAtm> newRemoteAtmFn;
+    private Function<String, RemoteAtm> newRemoteAtmFn;
     private Supplier <Department> newDepartmentFn;
 
-    public Factories(Supplier<RemoteAtm> newRemoteAtmFn, Supplier<Department> newDepartmentFn) {
+    public Factories(Function<String, RemoteAtm> newRemoteAtmFn, Supplier<Department> newDepartmentFn) {
         this.newRemoteAtmFn = newRemoteAtmFn;
         this.newDepartmentFn = newDepartmentFn;
     }
     
-    public RemoteAtm newRemoteAtm() {
-        return newRemoteAtmFn.get();
+    public RemoteAtm newRemoteAtm(String name) {
+        return newRemoteAtmFn.apply(name);
     }
 
     public Department newDepartment() {
