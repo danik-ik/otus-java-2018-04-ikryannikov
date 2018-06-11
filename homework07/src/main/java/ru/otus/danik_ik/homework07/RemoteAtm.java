c7477e8c6a147ebd933ee3edc8fd3853d735c81a
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-11 07:49:59 +0300

    переменная set переименована - уж очень неоднозначно выглядит

--- homework07/src/main/java/ru/otus/danik_ik/homework07/RemoteAtm.java
+++ homework07/src/main/java/ru/otus/danik_ik/homework07/RemoteAtm.java
@@ -3,7 +3,7 @@ package ru.otus.danik_ik.homework07;
 import java.math.BigDecimal;
 
 public interface RemoteAtm {
-    BoxSet replaceCurrencyBoxes(BoxSet set);
+    BoxSet replaceCurrencyBoxes(BoxSet boxSet);
     BigDecimal getAmountToIssue();
     BigDecimal getAmountTotal();
     String getName();
--- homework07/src/main/java/ru/otus/danik_ik/homework07/RemoteRecyclableATM.java
+++ homework07/src/main/java/ru/otus/danik_ik/homework07/RemoteRecyclableATM.java
@@ -19,18 +19,18 @@ public class RemoteRecyclableATM extends RecyclableATM implements RemoteAtm {
     }
 
     @Override
-    public BoxSet replaceCurrencyBoxes(BoxSet set) {
+    public BoxSet replaceCurrencyBoxes(BoxSet boxSet) {
         int inBoxesCount = this.getWithdrawBoxCount(); 
         WithdrawCurrencyBox[] outBoxes = 
-                new WithdrawCurrencyBox[ Math.max(set.getWithdrawBoxCount(), inBoxesCount) ];
+                new WithdrawCurrencyBox[ Math.max(boxSet.getWithdrawBoxCount(), inBoxesCount) ];
         
         // Что не влезает или не подходит по типу остаётся снаружи
         for (int i = 0; i < outBoxes.length; i++) {
-            outBoxes[i] = set.getWithdrawBox(i);
+            outBoxes[i] = boxSet.getWithdrawBox(i);
             if (i < inBoxesCount && outBoxes[i] instanceof RecyclableCurrencyBox)
                 outBoxes[i] = this.replaceRecyclableBox(i, (RecyclableCurrencyBox)outBoxes[i]);
         }
-        DepositAllDenominationsCurrencyBox dBox = replaceDepositBox(set.getDepositBox());
+        DepositAllDenominationsCurrencyBox dBox = replaceDepositBox(boxSet.getDepositBox());
                 
         return new BoxSet(dBox, outBoxes);
     }
