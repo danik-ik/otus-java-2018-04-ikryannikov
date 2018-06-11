16fc6698cfb92b81b2f254cef9693ea6c2d138e8
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-11 09:49:45 +0300

    Переработан класс фабрик. Добавлена фабрика кассет для выдачи

--- homework07/src/main/java/ru/otus/danik_ik/homework07/Main.java
+++ homework07/src/main/java/ru/otus/danik_ik/homework07/Main.java
@@ -1,36 +1,44 @@
 package ru.otus.danik_ik.homework07;
 
 import ru.otus.danik_ik.homework06.DefaultBundleFactory;
+import ru.otus.danik_ik.homework06.RecyclableCurrencyBoxImpl;
 import ru.otus.danik_ik.homework06.money.BundleFactory;
 
 import java.util.function.BiConsumer;
 
 public class Main
 {
-    private final Factories factories;
-
-    {
+    private final Factories factories = new Factories(){{
+        final int DEFAULT_CAPACITY = 3000;
         final BiConsumer <RemoteAtm, String> callbackHandler = (atm, message) -> {
-            System.out.printf("===[ %s ]=====================================\n", atm.getName());
+            System.out.printf("===[ %s ]=====================================\n", atm.getName() );
             System.out.println(message);
         };
 
         final BundleFactory bundleFactory = new DefaultBundleFactory();
 
-        factories = new Factories(
-                (name) ->
-                        new RemoteRecyclableATM(bundleFactory, name, callbackHandler),
-                () -> new SimpleDepartment()
-        );
-    }
-
-    private void run() {
-        RemoteAtm atm1 = factories.newRemoteAtm("First");
-        RemoteAtm atm2 = factories.newRemoteAtm("Second");
-        RemoteAtm atm3 = factories.newRemoteAtm("Third");
-    }
+        setNewRemoteAtmFn(
+                (name) -> new RemoteRecyclableATM(bundleFactory, name, callbackHandler) );
+        setNewDepartmentFn(
+                () -> new SimpleDepartment() );
+        setNewWithdrawBoxFn(
+                (denomination, count) -> new RecyclableCurrencyBoxImpl(
+                        denomination, DEFAULT_CAPACITY, count, bundleFactory));
+    }};
 
     public static void main( String[] args ) {
         new Main().run();
+    }
+
+    private void run() {
+        Department dept = factories.newDepartment();
+
+        RemoteAtm atm1 = factories.newRemoteAtm("First");
+        RemoteAtm atm2 = factories.newRemoteAtm("Second");
+        RemoteAtm atm3 = factories.newRemoteAtm("Third");
+
+        BoxSet boxSet1 = ...
+        dept.subcsribe(atm1, boxSet1);
+        ....
     }
 }
