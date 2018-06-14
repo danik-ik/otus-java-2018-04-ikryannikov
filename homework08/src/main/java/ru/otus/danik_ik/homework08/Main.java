88faa40913c9244047b3500ad53af7b722a583e4
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-14 21:01:28 +0300

    причёсан main()

--- homework08/src/main/java/ru/otus/danik_ik/homework08/Main.java
+++ homework08/src/main/java/ru/otus/danik_ik/homework08/Main.java
@@ -9,23 +9,6 @@ import java.util.Map;
 public class Main
 {
     public static void main( String[] args ) {
-        Object it = "1234\"5";
-
-//        Jzon jzon = new Jzon();
-//        System.out.println(jzon.toJson(it));
-//
-//        Gson gson = new Gson();
-//        System.out.println(gson.toJson(it));
-//
-//        JSONAware a = new JSONArray();
-//        for (int i: new int[]{1,2,4,8}) {
-//            ((JSONArray) a).add(i);
-//        }
-//        JSONObject o = new JSONObject();
-//        o.put("aaa", a);
-//        o.put("bbb", a);
-//        System.out.println(o.toJSONString());
-
         System.out.println(new Jzon().getJzonType("12345"));
         System.out.println(new Jzon().getJzonType(new Object()));
         System.out.println(new Jzon().getJzonType(new HashMap<String,String>()));
@@ -53,6 +36,10 @@ public class Main
         System.out.println(new Jzon().toJson(to));
         System.out.println("=== Gson ====");
         System.out.println(new Gson().toJson(to));
+
+        System.out.println();
+        System.out.println("=============================");
+        System.out.println("Тут, вообще-то, баловство одно. на самом деле всё в тестах");
 
     }
 }
