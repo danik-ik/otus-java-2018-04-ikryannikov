5448381713c1928f21885f30edc84bc3c9c38c94
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-14 21:30:10 +0300

    Удалён отладочный вывод

--- homework08/src/main/java/ru/otus/danik_ik/homework08/jzon/Jzon.java
+++ homework08/src/main/java/ru/otus/danik_ik/homework08/jzon/Jzon.java
@@ -127,7 +127,6 @@ public class Jzon {
             if (m.getDeclaringClass().equals(Object.class)) continue;
 
             Object returned = null;
-            System.out.println(m.getName());
             try {
                 returned = m.invoke(src);
             } catch (InvocationTargetException | IllegalAccessException e) {
