aecc1a4b3f8aa8e04951b7ff319469126ed849a1
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-18 20:33:06 +0300

    Определён ID для несохранённого объекта

--- homework09/src/main/java/ru/otus/danik_ik/homework09/storage/DataSet.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/storage/DataSet.java
@@ -3,7 +3,8 @@ package ru.otus.danik_ik.homework09.storage;
 import java.sql.SQLException;
 
 public abstract class DataSet {
-    private long ID;
+    public static final long UNDEFINED_ID = -1L;
+    private long ID = UNDEFINED_ID; // определяется при загрузке или по факту сохранения
 
     public long getID() {
         return ID;
