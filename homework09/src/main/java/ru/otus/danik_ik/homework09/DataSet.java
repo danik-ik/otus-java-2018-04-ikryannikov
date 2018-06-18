e8bf5eac8173804cde1a082c2dc8710c62c90f81
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-18 20:04:19 +0300

    Revert "Возможно, как-то так?"
    This reverts commit 05d351d55672d7a4ffe689b82e7500b660c383f0.

--- homework09/src/main/java/ru/otus/danik_ik/homework09/DataSet.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/DataSet.java
@@ -5,9 +5,6 @@ import java.sql.SQLException;
 public abstract class DataSet {
     private long ID;
 
-    public abstract <T extends DataSet> T load(long id) throws SQLException;
-    public abstract void save() throws SQLException;
-
     public long getID() {
         return ID;
     }
--- homework09/src/main/java/ru/otus/danik_ik/homework09/Executor.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/Executor.java
@@ -6,16 +6,8 @@ import java.sql.SQLException;
 
 public class Executor {
     <T extends DataSet> void save(T entry) throws SQLException {
-        entry.save();
     }
     <T extends DataSet> T load(long id, Class<T> clazz) throws SQLException {
-        T entry = null;
-        try {
-            entry = clazz.getConstructor().newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        entry.load(id);
-        return entry;
+        return null;
     }
 }
