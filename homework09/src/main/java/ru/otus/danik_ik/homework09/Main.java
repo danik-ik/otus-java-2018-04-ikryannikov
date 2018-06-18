c666b8ea16a42b2a6e491e25473e7e50ca494831
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-18 22:05:55 +0300

    Пробные запуски

--- homework09/src/main/java/ru/otus/danik_ik/homework09/Main.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/Main.java
@@ -1,8 +1,19 @@
 package ru.otus.danik_ik.homework09;
 
+import ru.otus.danik_ik.homework09.database.SqlExecutor;
+
 public class Main
 {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws Exception {
+        try (SqlExecutor executor = new SqlExecutor()) {
+            ResourceHelper rh = new ResourceHelper(Main.class);
+
+            String query = rh.getString("sql/Create_table_user.sql");
+            System.out.println(query);
+            executor.execUpdate(query);
+        }
+        try (SqlExecutor executor = new SqlExecutor()) {
+            executor.execQuery("select * from user", result -> System.out.println("QUERY OK"));
+        }
     }
 }
