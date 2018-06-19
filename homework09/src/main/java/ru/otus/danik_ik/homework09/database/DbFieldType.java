56086f3f0b88f892a27a99c1d79e9502518b2500
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-19 21:19:49 +0300

    К типу поля добавлен метод установки значения для PreparedStatement

--- homework09/src/main/java/ru/otus/danik_ik/homework09/database/DbFieldType.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/database/DbFieldType.java
@@ -1,8 +1,56 @@
 package ru.otus.danik_ik.homework09.database;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public enum DbFieldType {
-    LONG,
-    FLOAT,
-    STRING,
-    DATE,
+    LONG( (stmt, value, index) -> {
+                try {
+                    stmt.setLong(index, (Long)value);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+    ),
+    FLOAT( (stmt, value, index) -> {
+            try {
+                stmt.setFloat(index, (Float) value);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    ),
+    STRING( (stmt, value, index) -> {
+            try {
+                stmt.setString(index, (String)value);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    ),
+    DATE( (stmt, value, index) -> {
+            try {
+                stmt.setDate(index, (Date) value);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    );
+
+    private final PreparedStatementValSetter preparedStatementValSetter;
+
+    DbFieldType(PreparedStatementValSetter preparedStatementValSetter) {
+        this.preparedStatementValSetter = preparedStatementValSetter;
+    }
+
+    public PreparedStatementValSetter getPreparedStatementValSetter() {
+        return preparedStatementValSetter;
+    }
+
+    @FunctionalInterface
+    public interface PreparedStatementValSetter {
+        void set(PreparedStatement stmt, Object value, int index);
+    }
+
 }
