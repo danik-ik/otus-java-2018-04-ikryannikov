03616c3e913494c332ec2c30dbd667e65e316d03
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-19 20:29:47 +0300

    Аннотированы методы классов

--- homework09/src/main/java/ru/otus/danik_ik/homework09/UserDataSet.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/UserDataSet.java
@@ -1,5 +1,7 @@
 package ru.otus.danik_ik.homework09;
 
+import ru.otus.danik_ik.homework09.database.DbFieldType;
+import ru.otus.danik_ik.homework09.database.annotations.DbField;
 import ru.otus.danik_ik.homework09.database.annotations.DbTable;
 import ru.otus.danik_ik.homework09.storage.DataSet;
 
@@ -7,21 +9,26 @@ import java.util.Date;
 
 @DbTable(name = "Users")
 public class UserDataSet extends DataSet {
+    @DbField(name="name", type = DbFieldType.STRING)
     private String name;
     private Date bornDate;
 
+    @DbField(name="name", type = DbFieldType.STRING)
     public String getName() {
         return name;
     }
 
+    @DbField(name="name", type = DbFieldType.STRING)
     public void setName(String name) {
         this.name = name;
     }
 
+    @DbField(name="bornDate", type = DbFieldType.DATE)
     public Date getBornDate() {
         return bornDate;
     }
 
+    @DbField(name="bornDate", type = DbFieldType.DATE)
     public void setBornDate(Date bornDate) {
         this.bornDate = bornDate;
     }
--- homework09/src/main/java/ru/otus/danik_ik/homework09/storage/DataSet.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/storage/DataSet.java
@@ -1,15 +1,20 @@
 package ru.otus.danik_ik.homework09.storage;
 
+import ru.otus.danik_ik.homework09.database.DbFieldType;
+import ru.otus.danik_ik.homework09.database.annotations.DbField;
+
 import java.sql.SQLException;
 
 public abstract class DataSet {
     public static final long UNDEFINED_ID = -1L;
     private long ID = UNDEFINED_ID; // определяется при загрузке или по факту сохранения
 
+    @DbField(name="ID", type = DbFieldType.LONG, isKey = true)
     public long getID() {
         return ID;
     }
 
+    @DbField(name="ID", type = DbFieldType.LONG, isKey = true)
     /* package */ void setID(long ID) {
         this.ID = ID;
     }
