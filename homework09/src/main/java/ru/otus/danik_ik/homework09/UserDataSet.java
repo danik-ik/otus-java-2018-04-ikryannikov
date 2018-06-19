5bc5bcfc332cfdaeb26c1d63e0e05b44a86d1383
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-19 13:45:23 +0300

    В UserDataset добавлены геттеры и сеттеры

--- homework09/src/main/java/ru/otus/danik_ik/homework09/UserDataSet.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/UserDataSet.java
@@ -8,4 +8,19 @@ public class UserDataSet extends DataSet {
     private String name;
     private Date bornDate;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBornDate() {
+        return bornDate;
+    }
+
+    public void setBornDate(Date bornDate) {
+        this.bornDate = bornDate;
+    }
 }
