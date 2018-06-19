dd105cd7c6503e7d1e0f5a34ea2f52553a660d7a
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-19 22:10:11 +0300

    Умолчание для isKey

--- homework09/src/main/java/ru/otus/danik_ik/homework09/database/annotations/DbField.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/database/annotations/DbField.java
@@ -2,15 +2,12 @@ package ru.otus.danik_ik.homework09.database.annotations;
 
 import ru.otus.danik_ik.homework09.database.DbFieldType;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.FIELD})
 public @interface DbField {
     String name();
     DbFieldType type();
-    boolean isKey();
+    boolean isKey() default false;
 }
