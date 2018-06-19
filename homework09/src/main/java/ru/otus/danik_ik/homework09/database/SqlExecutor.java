298614a45ed4b0eb537182e998da5e2cf91389eb
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-19 20:30:06 +0300

    Первые наброски по сохранению

--- homework09/src/main/java/ru/otus/danik_ik/homework09/database/SqlExecutor.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/database/SqlExecutor.java
@@ -3,10 +3,13 @@ package ru.otus.danik_ik.homework09.database;
 import ru.otus.danik_ik.homework09.storage.DataSet;
 import ru.otus.danik_ik.homework09.storage.Executor;
 
+import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Collection;
+import java.util.LinkedList;
 
 public class SqlExecutor implements Executor {
     private final Connection connection;
@@ -40,7 +43,8 @@ public class SqlExecutor implements Executor {
 
     @Override
     public <T extends DataSet> void save(T entry) {
-
+        new DataSetSaver<T>(connection, entry)
+                .save();
     }
 
     @Override
@@ -51,5 +55,37 @@ public class SqlExecutor implements Executor {
     @Override
     public void close() throws Exception {
         connection.close();
+    }
+
+    private class DataSetSaver<T extends DataSet> {
+        private final Connection connection;
+        private final T source;
+
+        public DataSetSaver(Connection connection, T source) {
+            this.connection = connection;
+            this.source = source;
+        }
+
+        public void save() {
+            buildQuery();
+            applyParameters();
+            executePrepared();
+        }
+
+        private Collection<Method> rowGetters = new LinkedList<>();
+        private Collection<Method> keyGetters = new LinkedList<>();
+
+        private void buildQuery() {
+            // найти геттеры с аннотацией DbField
+            // Построить запрос, набрать коллекцию лямбд для установки значений
+        }
+
+        private void applyParameters() {
+        }
+
+        private void executePrepared() {
+        }
+
+        ;
     }
 }
