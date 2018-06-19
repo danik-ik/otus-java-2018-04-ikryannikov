c58dfc6f1576f051aefad0aee163a53b9c755cde
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-19 20:26:45 +0300

    Описана аннотация DbField

--- homework09/src/main/java/ru/otus/danik_ik/homework09/database/annotations/DbField.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/database/annotations/DbField.java
@@ -1,5 +1,16 @@
 package ru.otus.danik_ik.homework09.database.annotations;
 
+import ru.otus.danik_ik.homework09.database.DbFieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD})
 public @interface DbField {
-    
+    String name();
+    DbFieldType type();
+    boolean isKey();
 }
