0d69593e3631543a85bb981074caefb148674dc3
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-21 14:09:53 +0300

    Что-то как-то...

--- homework09/src/main/java/ru/otus/danik_ik/homework09/database/DataSetLoader.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/database/DataSetLoader.java
@@ -1,5 +1,6 @@
 package ru.otus.danik_ik.homework09.database;
 
+import ru.otus.danik_ik.homework09.database.annotations.DbField;
 import ru.otus.danik_ik.homework09.storage.DataSet;
 
 import java.lang.reflect.Method;
@@ -23,14 +24,52 @@ class DataSetLoader<T extends DataSet> {
     }
 
     private Collection<Method> nonKeySetters = new LinkedList<>();
-    private Collection<Method> keySetters = new LinkedList<>();
+    private Collection<Method> keyGetters = new LinkedList<>();
 
     private Map<String, SqlExecutor.ResultSetReader> NonKeyMappers = new HashMap<>();
-    private Map<String, SqlExecutor.ResultSetReader> keyMappers = new HashMap<>();
+    private Map<String, SqlExecutor.PreparedStatementObjSetter> keyMappers = new HashMap<>();
 
     private Consumer<PreparedStatement> setParamsFor;
 
     public T load() {
+        collectMethods();
+        buildMappers();
+
         return null;
+    }
+
+    private void collectMethods() {
+        for (Method m : clazz.getMethods()) {
+            if (isApplicableGetter(m)) {
+                rememberGetter(m);
+            }
+            if (isApplicableSetter(m)) {
+                rememberGetter(m);
+            }
+        }
+    }
+
+    private void rememberGetter(Method m) {
+        keyGetters.add(m);
+    }
+
+    private boolean isApplicableSetter(Method m) {
+        if (m.getParameterCount() != 1) return false;
+        if (!m.getName().startsWith("set")) return false;
+        if (m.getDeclaringClass().equals(Object.class)) return false;
+        if (m.getAnnotationsByType(DbField.class).length == 0) return false;
+        DbField anno = m.getAnnotationsByType(DbField.class)[0];
+        if (!anno.type().isCompatibleFor(m.getParameters()[0].getType())) return false;
+        return true;
+    }
+
+    private boolean isApplicableGetter(Method m) {
+        if (m.getParameterCount() > 0) return false;
+        if (!m.getName().startsWith("get")) return false;
+        if (m.getDeclaringClass().equals(Object.class)) return false;
+        if (m.getAnnotationsByType(DbField.class).length == 0) return false;
+        DbField anno = m.getAnnotationsByType(DbField.class)[0]; 
+        if (!anno.isKey()) return false;
+        return true;
     }
 }
