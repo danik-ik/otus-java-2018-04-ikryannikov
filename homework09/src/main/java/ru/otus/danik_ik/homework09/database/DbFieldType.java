8383862d4ff023d28b446ed437105af81cd881cc
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-21 14:03:37 +0300

    Добавлен параметр -- список совместимых классов

--- homework09/src/main/java/ru/otus/danik_ik/homework09/database/DbFieldType.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/database/DbFieldType.java
@@ -3,9 +3,14 @@ package ru.otus.danik_ik.homework09.database;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public enum DbFieldType {
-    LONG( (stmt, value, index) -> {
+    LONG( classArray(Long.class, long.class), 
+            (stmt, value, index) -> {
                 try {
                     stmt.setLong(index, (Long)value);
                 } catch (SQLException e) {
@@ -13,7 +18,8 @@ public enum DbFieldType {
                 }
             }
     ),
-    FLOAT( (stmt, value, index) -> {
+    FLOAT( classArray(Float.class, float.class),
+            (stmt, value, index) -> {
             try {
                 stmt.setFloat(index, (Float) value);
             } catch (SQLException e) {
@@ -21,7 +27,8 @@ public enum DbFieldType {
             }
         }
     ),
-    STRING( (stmt, value, index) -> {
+    STRING( classArray(String.class),
+            (stmt, value, index) -> {
             try {
                 stmt.setString(index, (String)value);
             } catch (SQLException e) {
@@ -29,7 +36,8 @@ public enum DbFieldType {
             }
         }
     ),
-    DATE( (stmt, value, index) -> {
+    DATE( classArray(LocalDate.class),
+            (stmt, value, index) -> {
             try {
                 java.sql.Date sqlDate = java.sql.Date.valueOf((LocalDate)value);
                 stmt.setDate(index, sqlDate);
@@ -40,8 +48,10 @@ public enum DbFieldType {
     );
 
     private final PreparedStatementValSetter preparedStatementValSetter;
+    private final Set<Class<?>> compatibleJavaClasses;
 
-    DbFieldType(PreparedStatementValSetter preparedStatementValSetter) {
+    DbFieldType(Set<Class<?>> compatibleJavaClasses, PreparedStatementValSetter preparedStatementValSetter) {
+        this.compatibleJavaClasses = compatibleJavaClasses;
         this.preparedStatementValSetter = preparedStatementValSetter;
     }
 
@@ -49,9 +59,18 @@ public enum DbFieldType {
         return preparedStatementValSetter;
     }
 
+    public boolean isCompatibleFor(Class<?> clazz) {
+        return compatibleJavaClasses.contains(clazz);
+    }
+
     @FunctionalInterface
     public interface PreparedStatementValSetter {
         void set(PreparedStatement stmt, Object value, int index);
     }
 
+    private static Set<Class<?>> classArray(Class<?>... classes) {
+        Set<Class<?>> result = new HashSet<>();
+        result.addAll(Arrays.asList(classes));
+        return result;
+    }
 }
