30994a6ea736cf02860461fa365c1a870f50185a
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-22 13:51:44 +0300

    К описанию типа поля добавлена лямбда для чтения поля с нужным классом

--- homework09/src/main/java/ru/otus/danik_ik/homework09/database/DbFieldType.java
+++ homework09/src/main/java/ru/otus/danik_ik/homework09/database/DbFieldType.java
@@ -1,6 +1,7 @@
 package ru.otus.danik_ik.homework09.database;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.Arrays;
@@ -16,43 +17,75 @@ public enum DbFieldType {
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
-            }
+            },
+            (resultSet, fieldName) -> {
+                try {
+                    return resultSet.getLong(fieldName);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            } 
     ),
     FLOAT( classArray(Float.class, float.class),
             (stmt, value, index) -> {
-            try {
-                stmt.setFloat(index, (Float) value);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+                try {
+                    stmt.setFloat(index, (Float) value);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            },
+            (resultSet, fieldName) -> {
+                try {
+                    return resultSet.getFloat(fieldName);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        }
     ),
     STRING( classArray(String.class),
             (stmt, value, index) -> {
-            try {
-                stmt.setString(index, (String)value);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+                try {
+                    stmt.setString(index, (String)value);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            },
+            (resultSet, fieldName) -> {
+                try {
+                    return resultSet.getString(fieldName);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        }
     ),
     DATE( classArray(LocalDate.class),
             (stmt, value, index) -> {
-            try {
-                java.sql.Date sqlDate = java.sql.Date.valueOf((LocalDate)value);
-                stmt.setDate(index, sqlDate);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+                try {
+                    java.sql.Date sqlDate = java.sql.Date.valueOf((LocalDate)value);
+                    stmt.setDate(index, sqlDate);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            },
+            (resultSet, fieldName) -> {
+                try {
+                    return resultSet.getDate(fieldName);
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
             }
-        }
     );
 
     private final PreparedStatementValSetter preparedStatementValSetter;
     private final Set<Class<?>> compatibleJavaClasses;
+    private final FieldGetter fieldGetter;
 
-    DbFieldType(Set<Class<?>> compatibleJavaClasses, PreparedStatementValSetter preparedStatementValSetter) {
+    DbFieldType(Set<Class<?>> compatibleJavaClasses, 
+                PreparedStatementValSetter preparedStatementValSetter, 
+                FieldGetter fieldGetter) {
         this.compatibleJavaClasses = compatibleJavaClasses;
         this.preparedStatementValSetter = preparedStatementValSetter;
+        this.fieldGetter = fieldGetter;
     }
 
     public PreparedStatementValSetter getPreparedStatementValSetter() {
@@ -63,9 +96,18 @@ public enum DbFieldType {
         return compatibleJavaClasses.contains(clazz);
     }
 
+    public FieldGetter getFieldGetter() {
+        return fieldGetter;
+    }
+
     @FunctionalInterface
     public interface PreparedStatementValSetter {
         void set(PreparedStatement stmt, Object value, int index);
+    }
+
+    @FunctionalInterface
+    public interface FieldGetter {
+        Object get(ResultSet resultSet, String fieldName);
     }
 
     private static Set<Class<?>> classArray(Class<?>... classes) {
