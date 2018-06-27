9b1f9df8917da0450fff08830845caf9b1190918
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-27 13:57:42 +0300

    Первичное заполнение реализации DbService на Hibernate

--- homework10/src/main/java/ru/otus/danik_ik/homework09etc/hibernateStorage/DbServiceImpl.java
+++ homework10/src/main/java/ru/otus/danik_ik/homework09etc/hibernateStorage/DbServiceImpl.java
@@ -1,6 +1,40 @@
 package ru.otus.danik_ik.homework09etc.hibernateStorage;
 
+import org.hibernate.SessionFactory;
+import ru.otus.danik_ik.homework09etc.UserDataSet;
 import ru.otus.danik_ik.homework09etc.storage.DBService;
 
+import java.util.List;
+
 public class DbServiceImpl implements DBService {
+    private final SessionFactory sessionFactory;
+
+    public DbServiceImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void save(UserDataSet dataSet) {
+        
+    }
+
+    @Override
+    public UserDataSet read(long id) {
+        return null;
+    }
+
+    @Override
+    public UserDataSet readByName(String name) {
+        return null;
+    }
+
+    @Override
+    public List<UserDataSet> readAll() {
+        return null;
+    }
+
+    @Override
+    public void close() throws Exception {
+        sessionFactory.close();
+    }
 }
