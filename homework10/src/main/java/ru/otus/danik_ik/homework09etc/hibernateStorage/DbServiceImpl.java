fd29eb3786918f19c8c3144d6080ae5b09ca5e3e
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-06-28 13:32:31 +0300

    Инициализация по умолчанию для DbServiceImpl (hibernate)

--- homework10/src/main/java/ru/otus/danik_ik/homework09etc/hibernateStorage/DbServiceImpl.java
+++ homework10/src/main/java/ru/otus/danik_ik/homework09etc/hibernateStorage/DbServiceImpl.java
@@ -1,16 +1,48 @@
 package ru.otus.danik_ik.homework09etc.hibernateStorage;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 import ru.otus.danik_ik.homework09etc.UserDataSet;
 import ru.otus.danik_ik.homework09etc.storage.DBService;
+import ru.otus.danik_ik.homework09etc.storage.dataSets.AddressDataSet;
+import ru.otus.danik_ik.homework09etc.storage.dataSets.PhoneDataSet;
 
 import java.util.List;
 
 public class DbServiceImpl implements DBService {
     private final SessionFactory sessionFactory;
 
+    public DbServiceImpl() {
+        Configuration configuration = new Configuration();
+
+        configuration.addAnnotatedClass(UserDataSet.class);
+        configuration.addAnnotatedClass(PhoneDataSet.class);
+        configuration.addAnnotatedClass(AddressDataSet.class);
+
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+        configuration.setProperty("hibernate.connection.driver_class", "org.h2.Driver");
+        configuration.setProperty("hibernate.connection.url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1");
+//        configuration.setProperty("hibernate.connection.username", "tully");
+//        configuration.setProperty("hibernate.connection.password", "tully");
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "create");
+        configuration.setProperty("hibernate.connection.useSSL", "false");
+        configuration.setProperty("hibernate.enable_lazy_load_no_trans", "true");
+
+        sessionFactory = createSessionFactory(configuration);
+    }
+
     public DbServiceImpl(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
+    }
+
+    private static SessionFactory createSessionFactory(Configuration configuration) {
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
+        builder.applySettings(configuration.getProperties());
+        ServiceRegistry serviceRegistry = builder.build();
+        return configuration.buildSessionFactory(serviceRegistry);
     }
 
     @Override
