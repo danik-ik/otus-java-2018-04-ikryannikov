32cf7707a83e370d7d836cfee55ed547b1d2f54e
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-07-06 14:13:00 +0300

    Только начал размечать зависимости

--- homework10/src/main/java/ru/otus/danik_ik/homework09etc/storage/dataSets/UserDataSet.java
+++ homework10/src/main/java/ru/otus/danik_ik/homework09etc/storage/dataSets/UserDataSet.java
@@ -1,16 +1,24 @@
 package ru.otus.danik_ik.homework09etc.storage.dataSets;
 
+import org.hibernate.annotations.Cascade;
 import ru.otus.danik_ik.homework09etc.hibernateStorage.convertors.LocalDateAttributeConverter;
 import ru.otus.danik_ik.homework09etc.storage.DataSet;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 public class UserDataSet extends DataSet {
     private String name;
     private LocalDate bornDate;
     private float rating;
+    
+    @OneToOne
+    private AddressDataSet address;
+    
+    @OneToMany
+    private List<PhoneDataSet> phones;
 
     public String getName() {
         return name;
