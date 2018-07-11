0af3180661e37112fb146fd81bf1ca28b2242fe0
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-07-11 14:24:19 +0300

    Изменена надпись, выводимая методом main

--- homework11/src/main/java/ru/otus/danik_ik/homework11/Main.java
+++ homework11/src/main/java/ru/otus/danik_ik/homework11/Main.java
@@ -3,6 +3,6 @@ package ru.otus.danik_ik.homework11;
 public class Main
 {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "There is NOTHING. See tests!" );
     }
 }
