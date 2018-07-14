33c367af3936ceb5685da10ec869a4ff5a963456
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-07-14 19:54:57 +0300

    Накидал классов из образца, с некоторыми доработками
    - TimerServlet исключён
    - Сервлет для авторизации не принимает параметров при создании (они там были только для демонстрации, что так можно)

--- homework12/src/main/java/ru/otus/danik_ik/homework12/Main.java
+++ homework12/src/main/java/ru/otus/danik_ik/homework12/Main.java
@@ -1,8 +1,30 @@
 package ru.otus.danik_ik.homework12;
 
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import ru.otus.danik_ik.homework12.servlet.AdminServlet;
+import ru.otus.danik_ik.homework12.servlet.LoginServlet;
+
 public class Main
 {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    private final static int PORT = 8090;
+    private final static String PUBLIC_HTML = "public_html";
+
+    public static void main( String[] args ) throws Exception {
+        ResourceHandler resourceHandler = new ResourceHandler();
+        resourceHandler.setResourceBase(PUBLIC_HTML);
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+
+        context.addServlet(LoginServlet.class, "/login");
+        context.addServlet(AdminServlet.class, "/admin");
+
+        Server server = new Server(PORT);
+        server.setHandler(new HandlerList(resourceHandler, context));
+
+        server.start();
+        server.join();
     }
 }
