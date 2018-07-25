4bfc6210899a3c810d76850495328942e388b439
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-07-25 14:12:50 +0300

    Немного попробовал с пулом потоков

--- homework14/src/main/java/ru/otus/danik_ik/homework14/Main.java
+++ homework14/src/main/java/ru/otus/danik_ik/homework14/Main.java
@@ -1,8 +1,25 @@
 package ru.otus.danik_ik.homework14;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static java.lang.Thread.yield;
+
 public class Main
 {
     public static void main( String[] args ) {
-        System.out.println( "There is NOTHING. See tests!" );
+        ExecutorService service = Executors.newFixedThreadPool(2);
+        try {
+            for(int i = 0; i < 10; i++)
+            {
+                int finalI = i;
+                service.submit(() -> {
+                    System.out.println(finalI);
+                    yield();
+                });
+            }
+        } finally {
+            service.shutdown();
+        }
     }
 }
