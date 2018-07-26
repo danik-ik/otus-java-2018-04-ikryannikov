260257d575b3c397c66e794624a5cf2c924a004a
Danila Ikryannikov <danila.ikryannikov@gmail.com>
2018-07-26 13:49:36 +0300

    Добавлена ещё куча тестовых параметров, приблизительно оценивается время выполнения

--- homework14/src/test/java/ru/otus/danik_ik/homework14/TreadSorterTest.java
+++ homework14/src/test/java/ru/otus/danik_ik/homework14/TreadSorterTest.java
@@ -20,16 +20,37 @@ public class TreadSorterTest
     @Parameterized.Parameters(name = "{index}: (size: {0}; threadCount: {1}; minQuamtity: {2})")
     public static Iterable<Object[]> dataForTest() {
         return Arrays.asList(new Object[][]{
-             {0, 1, 1},
-             {1, 1, 1},
-             {3, 1, 1},
-             {10, 1, 10},
-             {20, 1, 10},
-             {0, 3, 1},
-             {1, 3, 1},
-             {10, 9, 10},
-             {20, 9, 10},
-             {10000, 6, 33},
+                // размер массива, количество потоков, размер предварительно отсортированных фрагментов
+                {0, 1, 1},
+                {1, 1, 1},
+                {3, 1, 1},
+                {10, 1, 10},
+                {20, 1, 10},
+                {0, 3, 1},
+                {1, 3, 1},
+                {10, 9, 10},
+                {20, 9, 10},
+                {10000, 6, 33},
+                {1000000, 6, 33},
+                {1000000, 1, 100},
+                {1000000, 2, 100},
+                {1000000, 3, 100},
+                {1000000, 4, 100},
+                {1000000, 5, 100},
+                {1000000, 10, 100},
+                {1000000, 100, 100},
+                {10000, 1, 1},
+                {10000, 2, 1},
+                {10000, 3, 1},
+                {1000000, 2, 10000},
+                {1000000, 3, 10000},
+                {1000000, 4, 10000},
+                {1000000, 2, 100000},
+                {1000000, 3, 100000},
+                {1000000, 4, 100000},
+                {1000000, 2, 125000},
+                {1000000, 3, 125000},
+                {1000000, 4, 125000},
         });
     }
 
@@ -59,8 +80,17 @@ public class TreadSorterTest
 
     @Test
     public void testIt() throws Exception {
+        long startTime;
+        startTime = System.nanoTime();
         int[] result = ThreadSorter.sort(src, threadCount, minQuantity);
+        long stopTime;
+        stopTime = System.nanoTime();
+        System.out.println("TreadSorter sort duration: " + ((stopTime - startTime) *.000001D) + "ms");
+
+        startTime = System.nanoTime();
         Arrays.sort(src);
+        stopTime = System.nanoTime();
+        System.out.println("Arrays sort duration: " + ((stopTime - startTime) *.000001D) + "ms");
         assertArrayEquals(src, result);
     }
 }
